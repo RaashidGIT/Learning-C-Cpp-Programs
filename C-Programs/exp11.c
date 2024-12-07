bdaae0485d420bfcc63819964d19d12aa51816a4
RaashidGIT <148008217+RaashidGIT@users.noreply.github.com>
2024-12-07 19:50:44 +0530

    Update exp11.c

--- C-Programs/exp11.c
+++ C-Programs/exp11.c
@@ -1,9 +1,16 @@
+/*
+
+This code checks whether a given string is a palindrome. 
+A palindrome is a word or phrase that reads the same forward and backward (e.g., "racecar", "madam").
+
+*/
+
 #include <stdio.h>
 #include <string.h>
 
 int main() {
     char s[100];
-    int i, len, c = 0;
+    int i, len;
 
     printf("Enter string: ");
     scanf("%s", s);
@@ -11,17 +18,23 @@ int main() {
     len = strlen(s);
     
     for (i = 0; i < len / 2; i++) {
-        if (s[i] == s[len - i - 1]) {
-            c++;
+        if (s[i] != s[len - i - 1]) {
+            printf("String is not palindrome\n");
+            return 0;
         }
     }
-
-    if (c == len / 2) {
-        printf("String is palindrome\n");
-    } else {
-        printf("String is not palindrome\n");
-    }
-
+    printf("String is palindrome\n");
     return 0;
 }
 
+/*
+
+OUTPUT
+-------------------------------------------------------------
+Enter string: madam
+String is palindrome
+
+Enter string: hello
+String is not palindrome
+
+*/
