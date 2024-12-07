9499805f243847973eac34764f9020d4d275febb
RaashidGIT <148008217+RaashidGIT@users.noreply.github.com>
2024-12-07 20:56:29 +0530

    Update exp12.c

--- C-Programs/exp12.c
+++ C-Programs/exp12.c
@@ -1,45 +1,70 @@
+/*
+
+This code performs the following operations on a square matrix: Trace & Transpose.
+
+
+*/
+
 #include<stdio.h>
-void main(){
-	int a[10][10],t[10][10], i, j, n, sum;
-	printf("\nEnter the size of matrix: ");
-	scanf("%d", &n);
-	printf("\nEnter the values: ");
-	for(i = 0; i < n; i++)
-	{	
-		for(j = 0; j < n; j++)
-		{
-			scanf("%d", &a[i][j]);
-		}
-	}
-	printf("\n Given square matrix: ");
-	for(i = 0; i < n; i++)
-	{	
-		printf("\n");
-		for(j = 0; j < n; j++)
-		{
-			printf("%d\t", a[i][j]);
-		}
-	}
-	for(i = 0; i < n; i++)
-	{	
-		for(j = 0; j < n; j++)
-		{
-			t[j][i] = a[i][j];
-		}
-	}
-	printf("\nTranspose Matrix is: ");
-	for(j = 0; j < n; j++)
-	{	
-		printf("\n");
-		for(i = 0; i < n; i++)
-		{
-			printf("%d\t", t[j][i]);
-		}
-	}
-	sum = 0;
-	for(i = 0; i < n; i++)
-		sum = sum + a[i][i];
-	printf("\nTrace of Matrix is = %d\n", sum);
+
+void main() {
+    int a[10][10], t[10][10], n, sum = 0;
+    printf("\nEnter the size of matrix: ");
+    scanf("%d", &n);
+    
+    // Input the matrix
+    printf("\nEnter the values:\n");
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < n; j++)
+            scanf("%d", &a[i][j]);
+
+    // Print the original matrix
+    printf("\nGiven square matrix:\n");
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++)
+            printf("%d\t", a[i][j]);
+        printf("\n");
+    }
+
+    // Compute the transpose of the matrix
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < n; j++)
+            t[j][i] = a[i][j];
+
+    // Print the transpose matrix
+    printf("\nTranspose Matrix is:\n");
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++)
+            printf("%d\t", t[i][j]);
+        printf("\n");
+    }
+
+    // Calculate the trace of the matrix
+    for(int i = 0; i < n; i++)
+        sum += a[i][i];
+
+    printf("\nTrace of Matrix is = %d\n", sum);
 }
  
-	
+/*
+
+OUTPUT
+------------------------------------------------
+Enter the size of matrix: 3
+
+Enter the values:
+1 2 3 4 5 6 7 8 9
+
+Given square matrix:
+1	2	3	
+4	5	6	
+7	8	9	
+
+Transpose Matrix is:
+1	4	7	
+2	5	8	
+3	6	9	
+
+Trace of Matrix is = 15
+
+*/
