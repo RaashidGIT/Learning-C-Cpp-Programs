bf58a9bc9a521c8423fe88ca39e3cfea259fec85
RaashidGIT <148008217+RaashidGIT@users.noreply.github.com>
2024-12-08 08:11:11 +0530

    Update exp18.c

--- C-Programs/exp18/exp18.c
+++ C-Programs/exp18/exp18.c
@@ -1,3 +1,9 @@
+/*
+
+This C program reads and displays the contents of a file.
+
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -19,13 +25,23 @@ int main()
     }
     
     c = fgetc(fptr);
-    while (c != EOF)
+    while (c != EOF) // Loop to read the file until the end (EOF) is reached
     {
-        printf("%c", c);
-        c = fgetc(fptr);
+        printf("%c", c); // Print the current character
+        c = fgetc(fptr); // Read the next character from the file
     }
 
     fclose(fptr);
     return 0;
 }
 
+/*
+OUTPUT
+----------------------------------------------------------
+Enter the filename to open
+demo.txt
+
+Hello, world!
+This is a test file.
+
+*/
