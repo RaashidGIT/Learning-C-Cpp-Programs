3290a852dba2a0767d671c821872cfa15b1628f3
RaashidGIT <148008217+RaashidGIT@users.noreply.github.com>
2024-12-08 13:15:54 +0530

    Update prg1.c

--- C-Programs/Advanced-DataSturcture/prg1.c
+++ C-Programs/Advanced-DataSturcture/prg1.c
@@ -1,59 +1,113 @@
+/*
+
+This program implements a stack data structure using a static array in C. 
+A stack is a Last In, First Out (LIFO) data structure where the last element inserted is the first to be removed. 
+The program supports three main stack operations: Push, Pop, Display.
+
+*/
+
 #include<stdio.h>
 #include<stdlib.h>
-#define SIZE 10
+#define SIZE 10 
+// defines a constant named SIZE with a value of 10
+
+// Global Variables
 int stack[SIZE];
 int top = -1;
-void push(int x)
-{
-	if(top == SIZE - 1)
-		printf("\nStack overflow occur, Insertion not possible.");
-	else
-	{
-		top = top + 1;
-		stack[top] = x;
-		printf("\nInsertion successfull");
-	}
+
+// Function Prototypes
+void push(int x);
+void pop();
+void display();
+
+void main() {
+    int ch, value;
+    while(1) { // Creates an infinite loop of the MENU
+        printf("\n_____________MENU____________\n");
+        printf("\n1. Insert\n2. Delete\n3. Display\n4. Exit");
+        printf("\nEnter your choice: ");
+        scanf("%d", &ch);
+
+        switch(ch) {
+            case 1: 
+                printf("\nValue to be inserted: ");
+                scanf("%d", &value);
+                push(value);
+                break;
+
+            case 2: 
+                pop();
+                break;
+
+            case 3: 
+                display();
+                break;
+
+            case 4: 
+                printf("\nExiting program...\n");
+                exit(0);
+
+            default: 
+                printf("\nInvalid choice! Please try again.");
+        }
+    }
 }
-void pop()
-{
-	if(top == -1)
-		printf("\nStack underflow occur, deletion not possible");
-	else
-	{
-		int temp = stack[top];
-		top--;		
-		printf("\nDeleted element = %d", temp);
-	}
+
+// Function Definitions
+
+// Insert an element into the stack
+void push(int x) {
+    if(top == SIZE - 1)
+        printf("\nStack Overflow! Insertion not possible.");
+    else {
+        stack[++top] = x;
+        printf("\nInsertion successful.");
+    }
 }
-void display()
-{
-	int i;
-	printf("\nThe elements in the stack are:");
-	for(i = top; i >= 0; i--)
-		printf("%d  ", stack[i]);
+
+// Delete an element from the stack
+void pop() {
+    if(top == -1)
+        printf("\nStack Underflow! Deletion not possible.");
+    else {
+        printf("\nDeleted element = %d", stack[top--]);
+    }
 }
-void main()
-{
-	int ch, value;
-	while(1)
-	{
-		printf("\n_____________MENU____________\n");
-		printf("\n1.Insert\n2.Delete\n3.Display\n4.Exit");
-		printf("\nEnter your choice: ");
-		scanf("%d", &ch);
-		switch(ch)
-		{
-			case 1: printf("\nValue to be inserted is:");
-				scanf("%d", &value);
-				push(value);
-				break;
-			case 2: pop();
-				break;
-			case 3: display();
-				break;
-			case 4: exit(0);
-				break;
-			default: printf("\nInvalid choice!");
-		}
-	}
+
+// Display the stack elements
+void display() {
+    if(top == -1)
+        printf("\nThe stack is empty.");
+    else {
+        printf("\nThe elements in the stack are: ");
+        for(int i = top; i >= 0; i--)
+            printf("%d  ", stack[i]);
+    }
 }
+
+/*
+
+OUTPUT
+----------------------------------------------------------
+_____________MENU____________
+
+1.Insert
+2.Delete
+3.Display
+4.Exit
+Enter your choice: 1
+
+Value to be inserted is: 20
+Insertion successful
+
+_____________MENU____________
+
+1.Insert
+2.Delete
+3.Display
+4.Exit
+Enter your choice: 3
+
+The elements in the stack are: 20
+
+*/
