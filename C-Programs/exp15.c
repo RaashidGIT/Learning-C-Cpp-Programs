1d82315f78cf4ac4d07ff3e1c9d09971727fe35a
RaashidGIT <148008217+RaashidGIT@users.noreply.github.com>
2024-12-08 06:24:15 +0530

    Update exp15.c

--- C-Programs/exp15.c
+++ C-Programs/exp15.c
@@ -1,3 +1,13 @@
+/*
+
+This program checks whether a number is an Armstrong number.
+
+An Armstrong number is a number that equals the sum of its digits raised to the power of the number of digits.
+153 => (1^3) + (5^3) + (3^3) = 153 (Armstrong Number)
+9474 => (9^4) + (4^4) + (7^4) + (4^4) = 9474 (Armstrong Number)
+
+*/
+
 #include <stdio.h>
 #include <math.h>
 
@@ -9,7 +19,7 @@ int main() {
 
     // Counting digits
     while (n != 0) {
-        n /= 10;
+        n /= 10; // Remove the last digit
         digits++;
     }
 
@@ -18,9 +28,9 @@ int main() {
 
     // Armstrong number check
     while (n != 0) {
-        r = n % 10;
-        d = d + pow((double)r, (double)digits);
-        n = n / 10;
+        r = n % 10; // Extract the last digit
+        d = d + pow((double)r, (double)digits); // Add the digit raised to the power of the number of digits
+        n = n / 10; // Remove the last digit
     }
 
     // Output result
@@ -31,5 +41,14 @@ int main() {
 
     return 0;
 }
-// gcc exp15.c -lm 
+// gcc exp15.c -lm (Requires due to Math header file)
 
+/*
+
+OUTPUT
+-------------------------------------------------------------
+Enter a number: 153
+
+153 is an Armstrong number
+
+*/
