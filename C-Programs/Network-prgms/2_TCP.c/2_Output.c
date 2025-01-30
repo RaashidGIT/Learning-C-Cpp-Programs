225ae84a3d075984619561365bef5b3dc380bbf6
RaashidGIT <148008217+RaashidGIT@users.noreply.github.com>
2025-01-30 09:50:33 +0530

    Update and rename basic.c to 2_Output.c

---
+++ C-Programs/Network-prgms/2_TCP.c/2_Output.c
@@ -0,0 +1,15 @@
+-----------------------------------------------------------------------
+                                OUTPUT
+-----------------------------------------------------------------------
+
+Bind done
+
+Waiting for Incoming connection....
+
+Connection accepted
+
+From client:
+Hi
+
+To client:
+Hello!
