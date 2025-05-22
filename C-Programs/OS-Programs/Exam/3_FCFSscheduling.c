fb3405b33cadfa413aef1313ceea557eaa377be2
RaashidGIT <148008217+RaashidGIT@users.noreply.github.com>
2025-05-23 04:26:13 +0530

    Update 3_FCFSscheduling.c

--- C-Programs/OS-Programs/Exam/3_FCFSscheduling.c
+++ C-Programs/OS-Programs/Exam/3_FCFSscheduling.c
@@ -1,21 +1,16 @@
 #include <stdio.h>
 
 struct process {
-    int id;  
-    int arrival; 
-    int burst;   
-    int wait;    
-    int comp;  
-    int tottime; 
+    int id, wait, burst, tottime, comp, arrival;
 } p[20];
 
-int main() {
-    int n, i;
+void main() {
+    int i, n;
     float totwait = 0, totturnaround = 0, avwait, avturn;
-
+    
     printf("Enter the number of processes: ");
     scanf("%d", &n);
-
+    
     for (i = 0; i < n; i++) {
         printf("\nEnter process ID: ");
         scanf("%d", &p[i].id);
@@ -23,33 +18,32 @@ int main() {
         scanf("%d", &p[i].arrival);
         printf("Enter process burst time: ");
         scanf("%d", &p[i].burst);
-
         p[i].wait = 0;
-        p[i].comp = 0;
-        p[i].tottime = 0;
+        p[i].comp = 0; 
+        p[i].tottime = 0; 
     }
 
-    p[0].wait = 0;
-    p[0].comp = p[0].arrival + p[0].burst;
-
+    p[0].wait = 0; 
+    p[0].comp = p[0].arrival + p[0].burst;  
+    
     for (i = 1; i < n; i++) {
-        if (p[i].arrival > p[i - 1].comp) {
-            p[i].wait = 0;
-            p[i].comp = p[i].arrival + p[i].burst;
+        if (p[i].arrival > p[i-1].comp) {
+            p[i].wait = 0; 
         } else {
-            p[i].wait = p[i - 1].comp - p[i].arrival;
-            p[i].comp = p[i - 1].comp + p[i].burst;
+            p[i].wait = p[i-1].comp - p[i].arrival;
         }
+        
+        p[i].comp = p[i].arrival + p[i].wait + p[i].burst;
     }
 
     for (i = 0; i < n; i++) {
-        p[i].tottime = p[i].comp - p[i].arrival;
+        p[i].tottime = p[i].comp - p[i].arrival;  
         totwait += p[i].wait;
         totturnaround += p[i].tottime;
     }
 
-    avwait = totwait / n;
-    avturn = totturnaround / n;
+    avwait = totwait / n;  
+    avturn = totturnaround / n; 
 
     printf("\nID\tBurst Time\tArrival Time\tWait Time\tTurnaround Time\tCompletion Time");
     for (i = 0; i < n; i++) {
@@ -59,6 +53,4 @@ int main() {
 
     printf("\nAverage Waiting Time: %.1f", avwait);
     printf("\nAverage Turnaround Time: %.1f\n", avturn);
-
-    return 0;
 }
